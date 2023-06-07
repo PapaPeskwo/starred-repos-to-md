c36a6853a684d7b092d1e786ed55a3ebedad0bc9
PapaPeskwo <tomi.orlovac@gmail.com>
2023-06-07 12:17:38 +0300

    asks the user for output location instead. Also prints out the outputed file when done

--- starred_repos_to_md.py
+++ starred_repos_to_md.py
@@ -1,11 +1,12 @@
 import os
 import requests
 from pathlib import Path
+from path_converter import get_path, convert_path
 
 GITHUB_API_URL = "https://api.github.com"
 USERNAME = "<add_your_username"
 TOKEN = "<add_your_token>"
-OUTPUT_FILE = "<add_your_output_file.md>"
+FILENAME = "My_Starred_Repositories.md"
 
 def get_starred_repositories():
     starred_url = f"{GITHUB_API_URL}/users/{USERNAME}/starred"
@@ -20,19 +21,23 @@ def generate_markdown_table(repositories):
     return table
 
 def main():
+    output_dir = get_path()
+    output_dir = convert_path(output_dir)
+    output_file = Path(output_dir) / FILENAME
+    
     starred_repos = get_starred_repositories()
     markdown_table = generate_markdown_table(starred_repos)
 
     # Ensure the output directory exists
-    output_dir = Path(OUTPUT_FILE).parent
+    output_dir = Path(output_file).parent
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # Check if the file exists and add new entries if needed
-    if not os.path.exists(OUTPUT_FILE):
-        with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    if not os.path.exists(output_file):
+        with open(output_file, "w", encoding="utf-8") as f:
             f.write(markdown_table)
     else:
-        with open(OUTPUT_FILE, "r", encoding="utf-8") as f:
+        with open(output_file, "r", encoding="utf-8") as f:
             existing_content = f.read()
 
         new_content = ""
@@ -41,9 +46,11 @@ def main():
                 new_content += line + "\n"
 
         if new_content:
-            with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
+            with open(output_file, "a", encoding="utf-8") as f:
                 f.write(new_content)
-
+    
+    return output_file
 
 if __name__ == "__main__":
-    main()
+    output_file = main()
+    print(f'File created: {output_file}')
