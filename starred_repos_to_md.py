b61870811c0be322dc9a938d9cfcd9fd29f9175d
PapaPeskwo <tomi.orlovac@gmail.com>
2023-08-27 16:30:06 +0200

    loads variables from .env fixed bug where it did not get all starred repos pathing should work for all 3 major OS's

--- starred_repos_to_md.py
+++ starred_repos_to_md.py
@@ -1,18 +1,27 @@
 import os
 import requests
 from pathlib import Path
-from path_converter import get_path, convert_path
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 GITHUB_API_URL = "https://api.github.com"
-USERNAME = "<add_your_username"
-TOKEN = "<add_your_token>"
-FILENAME = "My_Starred_Repositories.md"
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
+FILENAME = os.getenv("FILENAME", "My_Starred_Repositories.md")
+
+def get_all_pages(url, headers):
+    results = []
+    while url:
+        response = requests.get(url, headers=headers)
+        results.extend(response.json())
+        url = response.links.get("next", {}).get("url")
+    return results
 
 def get_starred_repositories():
     starred_url = f"{GITHUB_API_URL}/users/{USERNAME}/starred"
     headers = {"Authorization": f"token {TOKEN}"}
-    response = requests.get(starred_url, headers=headers)
-    return response.json()
+    return get_all_pages(starred_url, headers)
 
 def generate_markdown_table(repositories):
     table = "| Repository | Description |\n| --- | --- |\n"
@@ -21,19 +30,17 @@ def generate_markdown_table(repositories):
     return table
 
 def main():
-    output_dir = get_path()
-    output_dir = convert_path(output_dir)
-    output_file = Path(output_dir) / FILENAME
+    output_file = Path(FILENAME)
     
     starred_repos = get_starred_repositories()
     markdown_table = generate_markdown_table(starred_repos)
 
     # Ensure the output directory exists
-    output_dir = Path(output_file).parent
+    output_dir = output_file.parent
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # Check if the file exists and add new entries if needed
-    if not os.path.exists(output_file):
+    if not output_file.exists():
         with open(output_file, "w", encoding="utf-8") as f:
             f.write(markdown_table)
     else:
@@ -48,7 +55,7 @@ def main():
         if new_content:
             with open(output_file, "a", encoding="utf-8") as f:
                 f.write(new_content)
-    
+
     return output_file
 
 if __name__ == "__main__":
